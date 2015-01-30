0eb8a83a50650659334be34f5765843531a8d763
Chris Belyea <chris@chrisbelyea.com>
2015-01-30 14:19:42 -0500

    Begin to parameterize knife.rb. See http://dougireton.com/blog/2013/03/17/a-knife-dot-rb-for-our-time/ for details

--- chef/knife.rb
+++ chef/knife.rb
@@ -1,6 +1,12 @@
 # See http://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
 
 current_dir = File.dirname(__FILE__)
+user_email  = `git config --get user.email`
+home_dir    = ENV['HOME'] || ENV['HOMEDRIVE']
+org         = ENV['chef_org'] || 'ssc'
+
+knife_override = "#{home_dir}/.chef/knife_override.rb"
+
 log_level                :info
 log_location             STDOUT
 node_name                "dtashner"
