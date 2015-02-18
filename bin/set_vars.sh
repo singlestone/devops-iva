f92766c33bd3f59d63f0a873aa99e8108e9fdbe0
Dave Tashner <dtashner@singlestoneconsulting.com>
2015-02-18 16:47:52 -0500

    adding support for AppDynamics. Your Twilio keys need to be added to set_vars.sh

--- bin/set_vars.sh
+++ bin/set_vars.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -v
-echo 'JAVA_OPTS="-Dtwilio.authToken=your_auth_token -Dtwilio.sid=your_sid"' >> /etc/tomcat7/tomcat7.conf
+echo 'JAVA_OPTS="-Dtwilio.authToken=your_auth_token -Dtwilio.sid=your_sid -javaagent:/opt/appdynamics/javaagent.jar"' >> /etc/tomcat7/tomcat7.conf
